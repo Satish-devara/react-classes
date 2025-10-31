10b124aaecb6ab850ebfa80080af57db359db7dd
Satish <sd1761256@gmail.com>
2025-10-31 20:18:33 +0530

    practice making stopwatch

--- React/first-app/src/App.jsx
+++ React/first-app/src/App.jsx
@@ -4,12 +4,13 @@ import Page from './Components/Page';
 import Count from './Components/Count';
 import Toggle from './Components/Toggle';
 import Stopwatch from './Components/Stopwatch';
+import Clock from './Components/Clock';
 function App() {
 
 
   return (
     <>
-      <Stopwatch />
+      <Clock />
     </>
   )
 }
